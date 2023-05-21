dad519cb43d2754727b570ae1fdf1241c9a6c680
jgq-cell <17318646019@163.com>
2023-05-21 11:40:57 +0800

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 取消测试github
 createApp(App).use(store).use(router).mount('#app')
