9bd51fa7a37615ee2ef13ef11cc33940a8bd0320
jgq-cell <17318646019@163.com>
2023-05-21 19:28:15 +0800

    feat: 项目基础配置完成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
+}
+
+// module.exports = {
+//   configureWebpack: (config) => {
+//     config.plugins.push(
+//       AutoImport({
+//         resolvers: [ElementPlusResolver()]
+//       })
+//     )
+//     config.plugins.push(
+//       Components({
+//         resolvers: [ElementPlusResolver()]
+//       })
+//     )
+//   }
+// }
